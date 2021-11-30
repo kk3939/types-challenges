71c04590a836a38e4197832979bf3ca43e6e4956
kk3939 <kyo51310@gmail.com>
2021-11-30 17:56:49 +0900

    first of array 回答完了

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 // 実行ファイル
 // 回答したらcommit
 
-const text = "Hello, world";
-console.log(text);
+type arr1 = ["a", "b", "c"];
+type arr2 = [3, 2, 1];
+
+type First<T extends unknown[]> = T extends [] ? never : T[0];
+
+type head1 = First<arr1>; // expected to be 'a'
+type head2 = First<arr2>; // expected to be 3
+
+const value1: head1 = "a";
+const value2: head2 = 3;
+console.log(value1);
+console.log(value2);
