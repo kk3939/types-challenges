6932f0f7812b7c626dfbca78216bee5d78338a91
kk3939 <kyo51310@gmail.com>
2021-11-30 18:37:58 +0900

    Length of tuple回答

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 // 実行ファイル
 // 回答したらcommit
+type tesla = ["tesla", "model 3", "model X", "model Y"];
+type spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT"
+];
+type Length<T extends unknown[]> = T extends [] ? never : T["length"];
+
+type teslaLength = Length<tesla>; // expected 4
+type spaceXLength = Length<spaceX>; // expected 5
