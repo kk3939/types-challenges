02eb290df27532d788a7ef33ba7f8c0b12c56c68
kk3939 <kyo51310@gmail.com>
2021-12-02 20:48:05 +0900

    include実装完了

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 // 実行ファイル
 // 回答したらcommit
 
-type Concat<T extends unknown[], K extends unknown[]> = [...T, ...K];
-
-type Result = Concat<[1], [2, 3, 4]>; // expected to be [1, 2]
-
-const array1 = [1, 2, 3];
-
-const array2 = [4, 5];
-
-const array3: Concat<typeof array1, typeof array2> = array1.concat(array2);
-
-console.log(array3);
+type Includes<T extends unknown[], K> = K extends T[number] ? true : false;
+type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">;
+// expected to be `false`
