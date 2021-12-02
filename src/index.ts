153f044ad18fc4501da97718c6c072653b27986b
kk3939 <kyo51310@gmail.com>
2021-12-02 20:22:27 +0900

    実行確認

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@
 type Concat<T extends unknown[], K extends unknown[]> = [...T, ...K];
 
 type Result = Concat<[1], [2, 3, 4]>; // expected to be [1, 2]
+
+const array1 = [1, 2, 3];
+
+const array2 = [4, 5];
+
+const array3: Concat<typeof array1, typeof array2> = array1.concat(array2);
+
+console.log(array3);
