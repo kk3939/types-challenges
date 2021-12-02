ed83414569c468677f6f59def4b14519b9cd2ce8
kk3939 <kyo51310@gmail.com>
2021-12-02 20:18:05 +0900

    concat実装完了

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 // 実行ファイル
 // 回答したらcommit
 
-type If<C extends true | false, T, F> = C extends true ? T : F;
+type Concat<T extends unknown[], K extends unknown[]> = [...T, ...K];
 
-const value: If<true, "a", "b"> = "a";
-
-console.log(typeof value);
+type Result = Concat<[1], [2, 3, 4]>; // expected to be [1, 2]
