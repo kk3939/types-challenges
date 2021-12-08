e78cfbdd57738c99f23626fe8c2df94a2c9e35d2
kk3939 <kyo51310@gmail.com>
2021-12-08 18:44:06 +0900

    get return type回答

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 // 実行ファイル
 // 回答したらcommit
+const fn = (v: boolean) => {
+  if (v) return 1;
+  else return 2;
+};
+
+type MyReturnType<T extends (...args: any[]) => unknown> = T extends (
+  ...args: any[]
+) => infer U
+  ? U
+  : never;
+
+type a = MyReturnType<typeof fn>; // should be "1 | 2"
