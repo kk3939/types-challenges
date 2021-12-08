c2d0c3ed802c4d5414a92a310b2ca19e3dd84487
kk3939 <kyo51310@gmail.com>
2021-12-08 18:24:21 +0900

    indexをclean

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,2 @@
 // 実行ファイル
 // 回答したらcommit
-
-type Awaited<T> = T extends Promise<infer U> ? U : never;
-
-const returnNumber = (): Promise<number> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(1);
-    }, 1000);
-  });
-};
-
-const main = async () => {
-  const x: Awaited<Promise<number>> = await returnNumber();
-  console.log(x);
-};
