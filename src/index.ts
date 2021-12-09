63b883c663266ee78eecdb36114492bb5ae2b247
kk3939 <kyo51310@gmail.com>
2021-12-09 09:50:30 +0900

    file clean aliasの練習 3行目

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,2 @@
 // 実行ファイル
 // 回答したらcommit
-
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
-}
-
-type MyOmit<T, K> = {
-  [P in Exclude<keyof T, K>]: T[P];
-};
-
-type TodoPreview = MyOmit<Todo, "description" | "title">;
-
-const todo: TodoPreview = {
-  completed: false,
-};
