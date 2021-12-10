d730670772cbd0810560d62c01e4cc7e7cb3610f
kk3939 <kyo51310@gmail.com>
2021-12-10 19:17:28 +0900

    readonly回答

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
 // 実行ファイル
 // 回答したらcommit
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type MyReadonly2<T, K extends keyof T> = {
+  readonly [P in K]: T[P];
+} & Omit<T, K>;
+
+const todo: MyReadonly2<Todo, "title" | "description"> = {
+  title: "Hey",
+  description: "foobar",
+  completed: false,
+};
+
+todo.title = "Hello"; // Error: cannot reassign a readonly property
+todo.description = "barFoo"; // Error: cannot reassign a readonly property
+todo.completed = true; // OK
