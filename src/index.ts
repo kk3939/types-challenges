9f2d79025ce8882847a525eb3d0d9e5c930f3678
kk3939 <kyo51310@gmail.com>
2021-12-27 16:45:01 +0900

    pop回答できた

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-type arr1 = ["a", "b", "c"];
+type arr1 = ["a", "b", "c", "d"];
 type arr2 = [3, 2, 1];
 
-type Last<T extends unknown[]> = T extends [...unknown[], infer L] ? L : never;
+type Pop<T extends any[]> = T extends [...infer V, ...infer L] ? V : never;
 
-type tail1 = Last<arr1>; // expected to be 'c'
-type tail2 = Last<arr2>; // expected to be 1
+type re1 = Pop<arr1>; // expected to be ['a', 'b', 'c']
+type re2 = Pop<arr2>; // expected to be [3, 2]
