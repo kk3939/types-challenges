22e8a084a55b296d693228a06b241d8f7be5f793
kk3939 <kyo51310@gmail.com>
2021-12-28 16:17:18 +0900

    Lookup回答

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-type arr1 = ["a", "b", "c", "d"];
-type arr2 = [3, 2, 1];
-
-type Pop<T extends any[]> = T extends [...infer V, ...infer L] ? V : never;
-
-type re1 = Pop<arr1>; // expected to be ['a', 'b', 'c']
-type re2 = Pop<arr2>; // expected to be [3, 2]
+type LookUp<U, T> = U extends Record<"type", string>
+  ? U["type"] extends T
+    ? U
+    : never
+  : never;
